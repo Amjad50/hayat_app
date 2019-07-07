38f4506bca7c5910d4b16f7fc41aa0725e3f571b
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-07-07 13:04:08 +0800

    Edit: changed the view of the cards to pageview
    - instead of slivers and padding,
    pageviews with columns is much better.

--- lib/pages/articles/articles.dart
+++ lib/pages/articles/articles.dart
@@ -29,7 +29,8 @@ DocumentSnapshot _fillNotFoundData(DocumentSnapshot snapshot) {
         .collection(ARTICLES_PAGES_COLLECTION)
         .document('default');
 
-  if (!snapshot.data.containsKey(TAGS) || !(snapshot.data[TAGS] is List<dynamic>))
+  if (!snapshot.data.containsKey(TAGS) ||
+      !(snapshot.data[TAGS] is List<dynamic>))
     snapshot.data[TAGS] = List<dynamic>();
 
   if (!snapshot.data.containsKey(MAINTITLE) ||
@@ -61,6 +62,64 @@ class _ArticlesPageState extends State<ArticlesPage> {
     );
   }
 
+  List<Widget> _buildPageViews(List<DocumentSnapshot> documents) {
+    // the best size for Nexus 5X is '6'
+    const _EACH_PAGE_COUNT = 8;
+
+    final list = <Widget>[];
+    if (documents.length >= 1) {
+      var first = _fillNotFoundData(documents[0]);
+      list.add(_buildArticleCardEntry(
+        ArticleData(
+            mainTitle: first[MAINTITLE],
+            articlePage: first[PAGEREF],
+            title: first[TITLE],
+            textColor: first[TEXTCOLOR],
+            img: first[IMG],
+            tags: first[TAGS],
+            heroTag: first.documentID),
+        large: true,
+      ));
+
+      int _generatePage (int oldIndex, int max) {
+        var childList = <Widget>[];
+        int i = oldIndex;
+        for (; i < max; i++) {
+          DocumentSnapshot current = _fillNotFoundData(documents[i]);
+          childList.add(_buildArticleCardEntry(
+            ArticleData(
+                mainTitle: current[MAINTITLE],
+                articlePage: current[PAGEREF],
+                heroTag: current.documentID,
+                title: current[TITLE],
+                textColor: current[TEXTCOLOR],
+                img: current[IMG],
+                tags: current[TAGS]),
+          ));
+        }
+        if(max - oldIndex < _EACH_PAGE_COUNT){
+          for (int i = max - oldIndex; i < _EACH_PAGE_COUNT; i++) {
+            // Padding for the last page
+            childList.add(Container());
+          }
+        }
+        list.add(Column(children: childList.map((e) => Expanded(child: e, flex: 1,)).toList()));
+        return i;
+      }
+
+      var currentI = 1;
+      for (var _ = 0; _ < (documents.length - 1) ~/ _EACH_PAGE_COUNT; _++) {
+        currentI = _generatePage(currentI, currentI + _EACH_PAGE_COUNT);
+      }
+
+      if (currentI < documents.length) {
+        _generatePage(currentI, documents.length);
+      }
+    }
+
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
@@ -72,44 +131,14 @@ class _ArticlesPageState extends State<ArticlesPage> {
           int count = snapshot.data.documents.length;
 
           if (count >= 1) {
-            var first = _fillNotFoundData(snapshot.data.documents[0]);
-            return CustomScrollView(slivers: <Widget>[
-              SliverFillViewport(
-                  delegate: SliverChildListDelegate([
-                _buildArticleCardEntry(
-                  ArticleData(
-                      mainTitle: first[MAINTITLE],
-                      articlePage: first[PAGEREF],
-                      title: first[TITLE],
-                      textColor: first[TEXTCOLOR],
-                      img: first[IMG],
-                      tags: first[TAGS],
-                      heroTag: first.documentID),
-                  large: true,
-                )
-              ])),
-              SliverPadding(
-                padding: EdgeInsets.all(20),
-              ),
-              SliverList(
-                  delegate: SliverChildBuilderDelegate(
-                (_, index) {
-                  var current =
-                      _fillNotFoundData(snapshot.data.documents[index + 1]);
-                  return _buildArticleCardEntry(
-                    ArticleData(
-                        mainTitle: current[MAINTITLE],
-                        articlePage: current[PAGEREF],
-                        heroTag: current.documentID,
-                        title: current[TITLE],
-                        textColor: current[TEXTCOLOR],
-                        img: current[IMG],
-                        tags: current[TAGS]),
-                  );
-                },
-                childCount: count - 1,
-              ))
-            ]);
+            final controller = PageController(initialPage: 0);
+            final children = _buildPageViews(snapshot.data.documents);
+
+            return PageView(
+              controller: controller,
+              scrollDirection: Axis.vertical,
+              children: children
+            );
           }
         }
 
