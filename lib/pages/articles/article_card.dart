2c3c30cc3e40b91a1b9b7cd4dfa160c125e65c0a
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-07-07 12:07:50 +0800

    Add: tags inside card implementation in the view
    - only appear in article view
    - similar to Curiosity app

--- lib/pages/articles/article_card.dart
+++ lib/pages/articles/article_card.dart
@@ -7,16 +7,19 @@ import 'package:hayat_app/utils.dart';
 class ArticleCard extends StatelessWidget {
   const ArticleCard(this.article, {Key key, this.large = false})
       : this._borderRadius = 10,
+        this._isInsideArticle = false,
         super(key: key);
 
   const ArticleCard.insideArticlePage(this.article, {Key key})
       : this._borderRadius = 0,
         this.large = true,
+        this._isInsideArticle = true,
         super(key: key);
 
   final ArticleData article;
   final bool large;
   final double _borderRadius;
+  final bool _isInsideArticle;
 
   @override
   Widget build(BuildContext context) {
@@ -27,16 +30,36 @@ class ArticleCard extends StatelessWidget {
             horizontal: 15, vertical: this.large ? 25 : 15),
         child: Material(
           color: Colors.transparent,
-          child: MarkdownBody(
-            data: mergeMarkdownArray(this.article.mainTitle),
-            // TODO: make global styleSheet
-            // TODO: fix bug overflow when transiting to article view
-            styleSheet: MarkdownStyleSheet(
-              p: TextStyle(
-                color: hexColor(this.article.textColor),
-                fontSize: this.large ? 40 : 20,
+          child: Column(
+            children: <Widget>[
+              MarkdownBody(
+                data: mergeMarkdownArray(this.article.mainTitle),
+                // TODO: make global styleSheet
+                // TODO: fix bug overflow when transiting to article view
+                styleSheet: MarkdownStyleSheet(
+                  p: TextStyle(
+                    color: hexColor(this.article.textColor),
+                    fontSize: this.large ? 40 : 20,
+                  ),
+                ),
               ),
-            ),
+              if (this._isInsideArticle)
+                Wrap(
+                  children: this
+                      .article
+                      .tags
+                      .map((e) => Padding(
+                        padding: const EdgeInsets.all(2.0),
+                        child: ActionChip(
+                            label: Text(e),
+                            //TODO: choose color?
+                            onPressed: () {
+                              //TODO implement a search based on tag?
+                            }),
+                      ))
+                      .toList(),
+                )
+            ],
           ),
         ),
         decoration: BoxDecoration(
