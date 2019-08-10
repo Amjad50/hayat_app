44954727d0419670745a3e97d275fc57532713fc
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-08-10 14:16:48 +0300

    Add: New task dialog
    - custom tasks, now can be created.

--- lib/pages/tasks/new_task_dialog.dart
+++ lib/pages/tasks/new_task_dialog.dart
@@ -11,22 +11,134 @@ class NewTaskDialog extends StatefulWidget {
 }
 
 class _NewTaskDialogState extends State<NewTaskDialog> {
+  final _formKey = new GlobalKey<FormState>();
+
+  String _name;
+  String _type;
+  double _durationH;
+
+  static const _white = const  TextStyle(color: Colors.white);
+
+  bool _validateAndSave() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
+  void _validateAndSubmit() {
+    if (_validateAndSave()) {
+      final task = TaskData(
+        name: _name,
+        type: _type,
+        durationH: _durationH,
+        tasksType: widget.tasksType,
+        done: 0,
+      );
+
+      Navigator.pop(context, task);
+    }
+  }
+
+  Widget _buildNameInput() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      maxLines: 1,
+      decoration: InputDecoration(
+        labelText: "Name",
+      ),
+      onSaved: (value) => _name = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Name cannot be empty";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildTypeInput() {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      maxLines: 1,
+      decoration: InputDecoration(
+        labelText: "Type",
+      ),
+      onSaved: (value) => _type = value,
+      validator: (value) {
+        if (value.isEmpty) {
+          return "Type cannot be empty";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildDurationInput() {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      maxLines: 1,
+      decoration: InputDecoration(
+        labelText: "Duration",
+      ),
+      onSaved: (value) => _durationH = double.parse(value),
+      validator: (value) {
+        if (value.isEmpty || double.parse(value) <= 0) {
+          return "Duration cannot be empty/zero/egative";
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildMainDialogBox() {
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        padding: EdgeInsets.only(bottom: 20.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            _buildNameInput(),
+            _buildTypeInput(),
+            _buildDurationInput(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSubmitButton() {
+    return RaisedButton(
+      onPressed: _validateAndSubmit,
+      child: const Text(
+        "SUBMIT",
+        style: _white,
+      ),
+    );
+  }
+
+  Widget _buildCancelButton() {
+    return RaisedButton(
+      onPressed: () {
+        Navigator.pop(context);
+      },
+      child: const Text(
+        "CANCEL",
+        style: _white,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      content: RaisedButton(
-        child: Text("New Entry"),
-        onPressed: () => Navigator.pop(
-          context,
-          TaskData(
-            tasksType: widget.tasksType,
-            name: "null",
-            type: "null",
-            durationH: 878787.0,
-            done: 20,
-          ),
-        ),
-      ),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+      title: const Text("New Task"),
+      content: _buildMainDialogBox(),
+      actions: <Widget>[_buildCancelButton(), _buildSubmitButton()],
     );
   }
 }
