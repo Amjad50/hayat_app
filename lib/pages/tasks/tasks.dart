153d1d73c4b21b1571dcf646447dc33562f86255
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-08-10 15:27:39 +0300

    Add: Choose the day to display the tasks at.
    - still the handling of firebase with a custom date (not implemented).

--- lib/pages/tasks/tasks.dart
+++ lib/pages/tasks/tasks.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hayat_app/pages/basepage.dart';
 import 'package:hayat_app/pages/tasks/tasks_collection_types.dart';
 import 'package:hayat_app/pages/tasks/tasks_handler.dart';
+import 'package:hayat_app/utils.dart';
 
 class TasksPage extends BasePage {
   TasksPage({Key key, String uid}) : super(key: key, uid: uid);
@@ -15,6 +16,8 @@ class _TasksPageState extends State<TasksPage> with TickerProviderStateMixin {
   List<TasksHandler> _taskshandlers;
   TabController _tabsController;
 
+  DateTime _choosenDay;
+
   @override
   void initState() {
     super.initState();
@@ -23,6 +26,7 @@ class _TasksPageState extends State<TasksPage> with TickerProviderStateMixin {
         .toList();
     _tabsController =
         TabController(length: widget.tabs.length, vsync: this, initialIndex: 1);
+    _choosenDay = DateTime.now();
   }
 
   @override
@@ -31,27 +35,83 @@ class _TasksPageState extends State<TasksPage> with TickerProviderStateMixin {
     super.dispose();
   }
 
-  Widget _buildTabs() {
-    final bar = TabBar(
+  PreferredSizeWidget _buildTabsBar() {
+    return TabBar(
       tabs: widget.tabs
           .map((e) => Tab(text: tasksCollectionTypesViewNames[e]))
           .toList(),
       controller: _tabsController,
     );
+  }
+
+  PreferredSizeWidget _buildDaysChoosedBar() {
     return PreferredSize(
-      child: Material(
-        color: Theme.of(context).accentColor,
-        child: bar,
-        elevation: 4, // TODO: remove elvation of the top appbar
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: InkWell(
+          onTap: () {
+            final now = DateTime.now();
+            showDatePicker(
+              context: context,
+              initialDate: _choosenDay,
+              firstDate: now.subtract(const Duration(days: 30)),
+              lastDate: now,
+            ).then((value) {
+              if(value != null) {
+                setState(() {
+                  _choosenDay = value;
+                });
+              }
+            });
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const Icon(
+                Icons.calendar_today,
+                size: 20,
+              ),
+              const Padding(
+                padding: const EdgeInsets.only(right: 4),
+              ),
+              Text(getDayDisplayString(_choosenDay)),
+              const Icon(Icons.arrow_drop_down)
+            ],
+          ),
+        ),
       ),
-      preferredSize: bar.preferredSize,
+      preferredSize: Size.fromHeight(50), // way larger than used (== infinite)
+    );
+  }
+
+  Widget _buildTopBar() {
+    final tabsBar = _buildTabsBar();
+    final daysChooseBar = _buildDaysChoosedBar();
+    return PreferredSize(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Material(
+            color: Theme.of(context).accentColor,
+            child: tabsBar,
+            elevation: 4, // TODO: remove elvation of the top appbar
+          ),
+          Material(
+            color: Colors.black26,
+            child: daysChooseBar,
+          ),
+        ],
+      ),
+      preferredSize: Size(tabsBar.preferredSize.width,
+          tabsBar.preferredSize.height + daysChooseBar.preferredSize.height),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _buildTabs(),
+      appBar: _buildTopBar(),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,13 +1,13 @@
+import 'package:intl/intl.dart';
 
 /// return the int in a String with a fixed size
 /// if its less, it will append leading zeros
 /// if its more, it will trunc from the left
 /// else: it will just return the String of int.
 String fixedSizeInt(int value, int size) {
-  if(value < 0)
-    return "-" + fixedSizeInt(-value, size - 1);
+  if (value < 0) return "-" + fixedSizeInt(-value, size - 1);
   String s = value.toString();
-  if(s.length < size)
+  if (s.length < size)
     return ("0" * (size - s.length)) + s;
   else
     return s.substring(s.length - size);
@@ -16,4 +16,30 @@ String fixedSizeInt(int value, int size) {
 /// this is used for the database access (FireStore)
 String getTasksDBDocumentName(DateTime date) {
   return "${fixedSizeInt(date.year, 4)}${fixedSizeInt(date.month, 2)}${fixedSizeInt(date.day, 2)}";
+}
+
+String dateToString(DateTime date, [DateTime now]) {
+  if (now == null) now = DateTime.now();
+  
+  final year = date.year == now.year ? "" : "y";
+
+  return DateFormat("${year}MMMEd").format(date);
+}
+
+String getDayDisplayString(DateTime date) {
+  final now = DateTime.now();
+  final diff = now.difference(date);
+
+  // future
+  if (diff.isNegative) return "Error";
+  print(date);
+  print(diff.inDays);
+  switch (diff.inDays) {
+    case 0:
+      return "Today";
+    case 1:
+      return "Yesterday";
+    default:
+      return dateToString(date);
+  }
 }
