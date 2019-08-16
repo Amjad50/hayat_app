2b19ad363aa4c6040f3f2d14f0014dc08ef6d393
Amjad Alsharafi <amjadsharafi10@gmail.com>
2019-08-16 21:36:11 +0300

    Add: ability to delete tasks and routine tasks

--- lib/pages/tasks/view/task_list_view.dart
+++ lib/pages/tasks/view/task_list_view.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:hayat_app/pages/tasks/task_data.dart';
 import 'package:hayat_app/pages/tasks/view/task_view.dart';
@@ -27,17 +28,30 @@ class _TasksListViewState extends State<TasksListView> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
+  void _delete() async {
+    final refernces = List<DocumentReference>(_selected.length);
+    int i = 0;
+    setState(() {
+      _selected.forEach((i) {
+        final task = widget.tasks.removeAt(i);
+        refernces[i++] = task.reference;
+      });
+      _selected.clear();
+    });
+    for (var ref in refernces) {
+      await ref.delete();
+    }
+  }
+
+  Widget _buildList() {
     return ListView.builder(
       itemBuilder: (context, index) {
         final taskView = TaskView(
-          data: widget.tasks[index],
-          onDoneChange: (value) {
-            widget.tasks[index].reference.updateData({DONE: value});
-          },
-          selected: _selected.contains(index)
-        );
+            data: widget.tasks[index],
+            onDoneChange: (value) {
+              widget.tasks[index].reference.updateData({DONE: value});
+            },
+            selected: _selected.contains(index));
 
         return GestureDetector(
           behavior: HitTestBehavior.opaque,
@@ -46,12 +60,53 @@ class _TasksListViewState extends State<TasksListView> {
             _select(index, !taskView.selected);
           },
           onTap: () {
-            if (_selected.isNotEmpty)
-              _select(index, !taskView.selected);
+            if (_selected.isNotEmpty) _select(index, !taskView.selected);
           },
         );
       },
       itemCount: widget.tasks.length,
+    );
+  }
+
+  Widget _buildDeleteButton() {
+    return RaisedButton.icon(
+      color: Colors.red.shade500,
+      icon: const Icon(Icons.delete),
+      label: const Text("Delete"),
+      onPressed: () {
+        _delete();
+      },
+    );
+  }
+
+  Widget _buildCrossAnimatedDeleteButton() {
+    return AnimatedCrossFade(
+      crossFadeState: _selected.isNotEmpty
+          ? CrossFadeState.showFirst
+          : CrossFadeState.showSecond,
+      duration: Duration(milliseconds: 500),
+      firstChild: Padding(
+        padding: const EdgeInsets.only(bottom: 40.0),
+        child: _buildDeleteButton(),
+      ),
+      secondChild: Container(),
+      layoutBuilder: (wtop, ktop, wbottom, kbottom) {
+        return Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[wbottom, wtop],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        _buildList(),
+        _buildCrossAnimatedDeleteButton(),
+      ],
     );
   }
 }
